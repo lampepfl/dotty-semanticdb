abab7ed319b4ef5406c4640058f7e8e874098b1c
Martin Odersky <odersky@gmail.com>
2019-09-16 18:07:55 +0200

    Adapt to new given syntax

--- src/main/scala/dotty/semanticdb/SemanticdbConsumer.scala
+++ src/main/scala/dotty/semanticdb/SemanticdbConsumer.scala
@@ -744,7 +744,7 @@ class SemanticdbConsumer(sourceFilePath: java.nio.file.Path) extends TastyConsum
 
       override def traverseTree(tree: Tree) given (ctx: Context): Unit = {
         tree match {
-          case Import(_, path, selectors) =>
+          case Import(path, selectors) =>
             val key = (tree.symbol.trueName, tree.pos.start)
             if (!packageDefinitions(key)) {
               packageDefinitions += key
